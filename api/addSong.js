b640c7d2d3fbbd9f0374fa0977822966621bb9c4
yaboialec <alec.dilanchian@gmail.com>
2022-09-24 11:37:00 -0400

    Adding filtering for weekly feels playlist + grabbing current playlist

--- api/addSong.js
+++ api/addSong.js
@@ -1,6 +1,49 @@
-const addSong = (req, res) => {
-  res.statusCode = 200;
-  res.json({ message: 'It works' });
-}
+import { getLatestFeelsPlaylist } from './helpers/spotify';
 
-export default addSong
+const addSong = async (req, res) => {
+  // "try catch deez nuts" - rushkiB [09.24.22]
+  try {
+    console.log('Calling addSong endpoint');
+    // Get latest playlist in list from Spotify
+    const currentPlaylist = await getLatestFeelsPlaylist();
+
+    if (!currentPlaylist) {
+      throw new Error('current playlist did not return.');
+    }
+
+    console.log(currentPlaylist);
+
+    const data = {
+      playlist: {
+        name: currentPlaylist.name,
+        currentPlaylist.uri,
+      },
+    };
+
+    /**
+     * PlaylistItem
+     * -> name
+     * -> uri (store this on apple watch)
+     */
+
+    // What data do we want to return on applewatch?
+
+    // Grab the current song being listened to on Spotify
+
+    // On successful find, add song to playlist
+
+    // Return number of songs in the playlist
+
+    res.statusCode = 200;
+    res.json({ data });
+  } catch (error) {
+    console.log('Ah shit.');
+    if (error.response && error.response.data) {
+      console.log(error.response.data);
+    } else {
+      console.log(error);
+    }
+  }
+};
+
+export default addSong;
---
+++ api/helpers/spotify.js
@@ -0,0 +1,113 @@
+require('dotenv').config();
+import FormData from 'form-data';
+const axios = require('axios').default;
+
+//-- Holds all of our helper functions for Spotify --//
+const BASE_URI = 'https://api.spotify.com/v1';
+
+const getAccessToken = async () => {
+  try {
+    const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
+    const response = await axios({
+      method: 'post',
+      params: {
+        grant_type: 'client_credentials',
+      },
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    // TODO: maybe should care about refresh_token
+    return response.data.access_token;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      console.log(`[getAccessToken] ${JSON.stringify(error.response.data)}`);
+    } else {
+      console.log(error);
+    }
+  }
+};
+
+const getAllPlaylists = async (accessToken) => {
+  try {
+    const { SPOTIFY_USER_ID } = process.env;
+    let playlistItems = [];
+    let nextURI = `${BASE_URI}/users/${SPOTIFY_USER_ID}/playlists?limit=50`;
+
+    do {
+      const response = await axios({
+        method: 'get',
+        url: nextURI,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // Check for next object, if here call again
+      if (response.data) {
+        const { next, items } = response.data;
+
+        // Put items in array
+        playlistItems = [...playlistItems, ...items];
+
+        // Set next && path
+        nextURI = next;
+      }
+    } while (nextURI);
+
+    console.log(`Number of items in array: ${playlistItems.length}`);
+    return playlistItems;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      console.log(`[getAllPlaylists] ${JSON.stringify(error.response.data)}`);
+    } else {
+      console.log(error);
+    }
+  }
+};
+
+const getLatestFeelsPlaylist = async () => {
+  // Get access token from spotify
+  const accessToken = await getAccessToken();
+
+  if (accessToken) {
+    // Grab every playlist on my account
+    const allPlaylists = await getAllPlaylists(accessToken);
+
+    // Filter all playlists by "Weekly Feels" naming + sort
+    const weeklyFeelsPlaylists = allPlaylists.filter((playlist) => {
+      const { name } = playlist;
+      const playlistNameToLower = name.toLowerCase();
+      const target = 'weekly feels';
+      console.log(`Checking ${playlistNameToLower}`);
+      return playlistNameToLower.includes(target);
+    });
+
+    console.log(`Number of weekly feels: ${weeklyFeelsPlaylists.length}`);
+
+    // Sort and grab the latest off the top
+    weeklyFeelsPlaylists.sort((p1, p2) => {
+      const p1Name = p1.name;
+      const p2Name = p2.name;
+      return (
+        parseInt(p1Name.split('#').pop()) - parseInt(p2Name.split('#').pop())
+      );
+    });
+
+    // Grab the latest playlist from the arry
+    return weeklyFeelsPlaylists.pop();
+  } else {
+    console.log('accessToken is borked loser');
+    return;
+  }
+};
+
+module.exports = {
+  getLatestFeelsPlaylist: getLatestFeelsPlaylist,
+};
